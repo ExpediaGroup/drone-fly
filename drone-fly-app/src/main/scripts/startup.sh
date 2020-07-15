a48d6ba977345da71dbc98ecff0f2ff69a1a399a
Abhimanyu Gupta <abhimanyu.gupta07@gmail.com>
2020-07-15 17:50:11 +0100

    removing startup.sh script

--- drone-fly-app/src/main/scripts/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ ! -z $DRONEFLY_CONFIG ]]; then
-  echo "$DRONEFLY_CONFIG"|base64 -d > ./conf/dronefly-config.yml
-  java -Dloader.path=/app/lib/ -jar /app/libs/drone-fly-app-$APP_VERSION-exec.jar --config=./conf/dronefly-config.yml
-else
-  java -Dloader.path=/app/lib/ -jar /app/libs/drone-fly-app-$APP_VERSION-exec.jar --apiary.bootstrapservers=$KAFKA_BOOTSTRAP_SERVERS --apiary.kafka.topicname=$KAFKA_TOPIC_NAME
-fi
